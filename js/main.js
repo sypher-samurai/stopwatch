10f175a0ab219f4efad0a6df1c0f338a0d86729e
sypher-samurai <spoiltcooker@gmail.com>
2020-04-06 20:07:03 +0100

    Done

--- js/main.js
+++ js/main.js
@@ -3,15 +3,18 @@ var toggleBtn = document.getElementById("toggle");
 var resetBtn = document.getElementById("reset");
 
 var watch = new Stopwatch(timer);
-
-toggleBtn.addEventListener("click", function () {
-  if (watch.isOn) {
-    watch.stop();
-    toggleBtn.textContent = "Start"
-  } else {
+function start(){
     watch.start();
     toggleBtn.textContent = "Stop"
-  }
+};
+function stop(){
+    watch.stop();
+    toggleBtn.textContent = "Start"
+}
+
+toggleBtn.addEventListener("click", function () {
+  (watch.isOn) ? stop() :  start();
+    
 });
 resetBtn.addEventListener("click", function(){
 watch.reset();
