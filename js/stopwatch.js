1d88633e20e45619d618f8da0704d7dd890a8e8d
sypher-samurai <spoiltcooker@gmail.com>
2020-04-06 19:11:54 +0100

    Progress

---
+++ js/stopwatch.js
@@ -0,0 +1,55 @@
+function Stopwatch() {
+  var time = 0;
+  var interval;
+  var offset;
+
+  function update() {
+    time += delta();
+    var formattedTime = timeFormatter(time);
+    console.log(formattedTime);
+  }
+  function delta() {
+    var now = Date.now();
+    var timePassed = now - offset;
+    offset = now;
+    return timePassed;
+  }
+  function timeFormatter(timeinMilliseconds) {
+    var time = new Date(timeinMilliseconds);
+    var minutes = time.getMinutes().toString();
+    var seconds = time.getSeconds().toString();
+    var milliseconds = time.getMilliseconds().toString();
+
+    if (minutes.length < 2) {
+      minutes = "0" + minutes;
+    }
+
+    if (seconds.length < 2) {
+      seconds = "0" + seconds;
+    }
+
+    while (milliseconds.length < 3) {
+      milliseconds = "0" + milliseconds;
+    }
+    return minutes + " : " + seconds + " : " + milliseconds;
+  }
+
+  this.isOn = false;
+  this.start = function () {
+    if (!this.isOn) {
+      interval = setInterval(update, 10);
+      offset = Date.now();
+      this.isOn = true;
+    }
+  };
+  this.stop = function () {
+    if (this.isOn) {
+      clearInterval(interval);
+      interval = null;
+      this.isOn = false;
+    }
+  };
+  this.reset = function () {
+    time = 0;
+  };
+}
